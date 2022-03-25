2142fc23685ba8317cffcc4151343f48885aef16
ulan <argymak@unist.ac.kr>
2022-03-25 18:45:27 +0900

    Plotting in nfr

--- nfr.py
+++ nfr.py
@@ -3,6 +3,8 @@ import pandas as pd
 from scipy import stats
 from scipy.stats import uniform
 import math
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
 
 LESS = 'LESS'
 MODERATE = 'MODERATE'
@@ -22,7 +24,78 @@ assemblyTypes = [
     HALFCONSOL
 
 ]
+
+nfx1for = {
+    LESS: 20,
+    MODERATE: 55.561,
+    MORE: 75
+}
+
+nfx2for = {
+    LESS: 2,
+    MODERATE: 13.33,
+    MORE: 15
+}
+
+nfx3for = {
+    LESS: 20000,
+    MODERATE: 30000,
+    MORE: 40000,
+}
+
+
 #%% Main function
+def plot_infor(hook_type, nfr1_range, nfr2_range, nfr3_range):
+    fit_value = {
+        CONSOL: (1, 2, 3)
+    }
+    fit_alpha, fit_loc, fit_beta = fit_value[hook_type]
+    b1=nfx1for[nfr1_range]
+
+    time_range = np.linspace(0, 50, 100)
+
+    gamma_pdf = stats.gamma.pdf(time_range, fit_alpha, fit_loc, fit_beta)
+    fig, ax = plt.subplots()
+    ax.plot(time_range, gamma_pdf, 'black', linewidth=2, label='Gamma Distribution')
+    ax.set_ylim(bottom=0)
+    plt.legend()
+
+    # Shaded region
+    a=0
+    ix = np.linspace(a, b1)
+    iy = stats.gamma.pdf(ix, fit_alpha, fit_loc, fit_beta)
+    verts = [(a, 0), *zip(ix, iy), (b1, 0)]
+    poly = Polygon(verts, facecolor='azure', edgecolor='0.5')
+    ax.add_patch(poly)
+
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    ax.xaxis.set_ticks_position('bottom')
+
+    #ax.set_xticks([a, b1], labels=['$0$', b1])
+    plt.xlabel("Assembly time (s)", fontsize=12)
+    plt.ylabel("Probability \n Density", rotation='horizontal', fontsize=12)
+    ax.yaxis.set_label_coords(-0.05, 1)
+
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def infor_con(hook_type, nfr1_range, nfr2_range, nfr3_range):
     p_nFR1, p_nFR2, p_nFR3 = 0, 0, 0
     assert assemblyTypes.__contains__(hook_type), "Assembly type does not match"
@@ -83,12 +156,8 @@ def infor_con(hook_type, nfr1_range, nfr2_range, nfr3_range):
 
 #%% Finding common range of nFR1
 def nFR1(nfr_range, data):
-    nf1x = {
-        LESS:20,
-        MODERATE:55.561,
-        MORE: 75
-    }
-    x = nf1x[nfr_range]
+
+    x = nfx1for[nfr_range]
 
     fit_alpha, fit_loc, fit_beta = stats.gamma.fit(data.Duration)
     p_nFR1 = stats.gamma.cdf(x=x, a=fit_alpha, loc=fit_loc, scale=fit_beta)
@@ -98,12 +167,8 @@ def nFR1(nfr_range, data):
 
 #%% Finding common range of nFR2
 def nFR2(nfr_range, data):
-    nfr2xs={
-        LESS : 2,
-        MODERATE : 13.33,
-        MORE : 15
-    }
-    x = nfr2xs[nfr_range]
+
+    x = nfx2for[nfr_range]
 
     params = stats.lognorm.fit(data.L1)
     p_nFR2 = stats.lognorm.cdf(x=x, s=params[0], loc=params[1], scale=params[2])
@@ -113,12 +178,8 @@ def nFR2(nfr_range, data):
 
 #%% nFR3_half_consol
 def nFR3_half_consol(nfr_range):
-    xs={
-        LESS:20000,
-        MODERATE:30000,
-        MORE:40000,
-    }
-    x=xs[nfr_range]
+
+    x=nfx3for[nfr_range]
 
     p_nFR3 = uniform.cdf(x=x, loc=19932.25, scale=(48762.273-19932.25))\
                  - uniform.cdf(x=19932.25, loc=19932.25, scale=(48762.273-19932.25))
@@ -127,12 +188,8 @@ def nFR3_half_consol(nfr_range):
 
 
 def nFR3_consol(nfr_range):
-    xs={
-        LESS:20000,
-        MODERATE:30000,
-        MORE:40000,
-    }
-    x=xs[nfr_range]
+
+    x=nfx3for[nfr_range]
     p_nFR3 = uniform.cdf(x=x, loc=31741.781, scale=(43171.249-31741.781))\
                  - uniform.cdf(x=31741.781,loc=31741.781, scale=(43171.249-31741.781))
 
