ca705dacad5177ef773542ad221034974a10b202
ulan <argymak@unist.ac.kr>
2022-03-31 15:47:11 +0900

    nFR3-Uniform plot finished

--- Plot Gamma.py
+++ Plot Gamma.py
@@ -2,33 +2,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 from matplotlib.patches import Polygon
-from scipy.stats import uniform
-
+from scipy.stats import gamma
+#%% Fitting values
 
 #%% Plotting gamma distribution
 
-def system_range_gamma_pdf(min, max):
-    y = gamma.pdf(x, fit_alpha, fit_loc, fit_beta)
-    hook_gamma_pdf=stats.gamma.cdf(x=55.5605,a=fit_alpha, loc=fit_loc, scale=fit_beta)
+def find_gamma_prob(drange, data):
+    fit_shape, fit_loc, fit_scale = stats.gamma.fit(data.Duration)
+    hook_gamma_prob=stats.gamma.cdf(x=drange,a=fit_shape, loc=fit_loc, scale=fit_scale)
 
-    return hook_gamma_pdf
+    return hook_gamma_prob
 
-def plot_unifor(min_srange,max_srange, min_drange, drange):
-    pdf_half_consol_10_20 = system_range_gamma_pdf(min_srange, max_srange)
-    x, y = uniform_step_axis(min_srange, max_srange, pdf_half_consol_10_20, tol=1000, num_points=1000)
+def plot_gamma(fit_shape, fit_loc, fit_scale, gamma_drange):
+    common_range = find_gamma_prob(gamma_drange, data)
+    x, y = graph_of_actual_gamma(fit_shape, fit_loc, common_range, tol=1000, num_points=1000)
 
     fig, ax = plt.subplots()
     ax.plot(x, y, 'black', linewidth=2, label='Uniform Distribution')
     ax.set_ylim(bottom=0)
-    ax.set_ylim(0, pdf_half_consol_10_20 * 2)
+    ax.set_ylim(0, common_range * 2)
     plt.legend()
 
     # Shaded region
-    ix = np.linspace(min_drange, drange)
-    iy = stats.uniform.pdf(x=ix, loc=min_srange, scale=(max_srange - min_srange))
+    ix = np.linspace(fit_scale, gamma_drange)
+    iy = stats.gamma.pdf(ix,fit_shape,fit_loc,fit_scale)
     # iy=iy*np.ones(ix.shape)
     iy = np.max(iy) * np.ones(ix.shape)
-    verts = [(min_drange, 0), *zip(ix, iy), (drange, 0)]
+    verts = [(0, 0), *zip(ix, iy), (gamma_drange, 0)]
     poly = Polygon(verts, facecolor='azure', edgecolor='0.5')
     ax.add_patch(poly)
 
@@ -38,12 +38,8 @@ def plot_unifor(min_srange,max_srange, min_drange, drange):
     plt.show()
 
 
-def uniform_step_axis(start, end, value=1,tol=10000,num_points=100):
+def graph_of_actual_gamma(start, end, tol=10000, num_points=100):
     x = np.linspace(start-tol,end+tol,num_points)
-    y= uniform_step(x,start,end, value)
+    y= stats.gamma.pdf(x,start,end, value)
     return x,y
 
-def uniform_step(x, start, end, value=1):
-    up=np.heaviside(x - start, 0)
-    down=np.heaviside(end - x, 0)
-    return up * down * value
